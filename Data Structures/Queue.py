2a2ec8341757feb2dce509783e98cae4ea2b40d7
Andrew Fales <andrewfales@hotmail.com>
2017-10-16 15:22:11 -0500

    Added a Queue class to start

---
+++ Data Structures/Queue.py
@@ -0,0 +1,45 @@
+from collections import deque
+class Queue():
+    def __init__(self,start=None):
+        if start is None:
+            start = []
+
+        self._length = len(start)
+        self._items = deque(start)
+
+    def __len__(self):
+        return self._length
+
+    def __str__(self):
+        return "[%s]" % (' '.join(map(str,self._items)))
+
+    def enqueue(self,item):
+        self._items.append(item)
+        self._length += 1
+
+    def dequeue(self):
+        if self._length == 0:
+            raise RuntimeError("Empty Queue")
+        self._length -= 1
+        return self._items.popleft()
+
+
+def main():
+    print("Testing Queue class...")
+    q = Queue()
+    print("Queue is initially empty")
+    print(q)
+
+    print("Adding values 1,2")
+    q.enqueue(1)
+    q.enqueue(2)
+    print(q)
+
+    print("Dequeueing one value")
+    item = q.dequeue()
+    print("Item dequeued: %s" % (item))
+    print(q)
+
+
+if __name__ == '__main__':
+    main()
