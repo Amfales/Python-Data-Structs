9c14b322b2282065b6d5f05079048f8ca446eca0
Andrew Fales <andrewfales@hotmail.com>
2017-10-17 14:09:33 -0500

    Skeleton for a Tree class

---
+++ Data Structures/Tree.py
@@ -0,0 +1,16 @@
+class Tree():
+    def __init__(self,parent=None):
+        self._p = parent
+        self._c = []
+
+
+class Node():
+    def __init__(self,data=None,parent=None):
+        self._d = data
+        self._p = parent
+        self._c = []
+
+
+
+if __name__ == '__main__':
+    main()
